a76c4ce6c977fd3e551cdc5c6839cb22a44d2c90
MaccedoFilho <ricardomacedor@icloud.com>
2025-05-30 16:56:46 -0300

    feat: configura GSAP com plugins essenciais para animações do portfólio

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,22 @@
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { SplitText } from "gsap/SplitText";
+import { CustomEase } from "gsap/CustomEase";
+
+gsap.registerPlugin(
+  Flip,
+  ScrambleTextPlugin,
+  ScrollTrigger,
+  ScrollSmoother,
+  ScrollToPlugin,
+  SplitText,
+  CustomEase
+);
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
